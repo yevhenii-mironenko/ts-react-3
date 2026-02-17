c634fbc40858dac917c81de20bcc4fc8ecdfe9c4
Yevhenii Myronenko <jackmironenko@gmail.com>
2026-02-17 20:34:06 +0200

    feat: implement countdown timer functionality in Timer component with progress display

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,36 @@
 import type { Timer } from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: Timer) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+
+    interval.current = timer;
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration} seconds</p>
+      <p>
+        <progress value={remainingTime} max={duration * 1000}></progress>
+      </p>
+      <p>{formattedRemainingTime} seconds</p>
     </Container>
   );
 }
