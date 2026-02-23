5dc6138b034f29b7dd232b9e4e5fe81290716a9e
Yevhenii Myronenko <jackmironenko@gmail.com>
2026-02-23 21:42:16 +0200

    feat: update Timer component to manage countdown based on timer context state

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,26 +1,33 @@
 import type { Timer } from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 import { useEffect, useRef, useState } from "react";
+import { useTimersContext } from "../store/timers-context.tsx";
 
 export default function Timer({ name, duration }: Timer) {
   const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
 
   if (remainingTime <= 0 && interval.current) {
     clearInterval(interval.current);
   }
 
   useEffect(() => {
-    const timer = setInterval(function () {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
-
-    interval.current = timer;
+    if (isRunning) {
+      const timer = setInterval(function () {
+        setRemainingTime((prevTime) => prevTime - 100);
+      }, 100);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
 
     return () => {
-      clearInterval(timer);
+      if (interval.current) {
+        clearInterval(interval.current);
+      }
     };
-  }, []);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
